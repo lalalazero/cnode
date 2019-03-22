418a5a8702a45b24efb6b38b8419d474f10b39bb
lalalazero <zzzero520@hotmail.com>
2019-03-22 15:13:54 +0800

    postlist

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
     <Header />
+    <main>
+      <PostList />
+    </main>
+    
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import PostList from './components/PostList.vue'
 export default {
   name: 'App',
   components: {
     Header,
+    PostList
   }
 }
 </script>
 
-<style>
+<style scoped>
+  #app {
+    background-color: #ddd;
+  }
+  main {
+    width: 80%;
+    margin: auto;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
           <li><a href="#">API</a></li>
           <li><a href="#">关于</a></li>
           <li><a href="#">注册</a></li>
-          <li><a href="#">登陆</a></li>
+          <li><a href="#">登录</a></li>
       </ul>
   </div>
 </template>
